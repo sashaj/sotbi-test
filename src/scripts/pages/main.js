86adf5f25adcaed21819012cb70d31bd0ee0ce9a
git_adf <alexanderivanovset@gmail.com>
2025-08-14 21:48:12 +0900

    add smooth corners

--- src/scripts/pages/main.js
+++ src/scripts/pages/main.js
@@ -4,15 +4,11 @@ import { InputMask } from '../components/inputMask.js';
 import { FormValidator } from '../components/validation.js';
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
 	new InputMask();
 	new FormValidator('.appointment.form');
 
-
 	document.querySelectorAll('.select').forEach($select => {
 		new Select($select);
 	});
-
-
 });
